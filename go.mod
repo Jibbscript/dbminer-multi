b739ac0d3fa2ac2732cff3ac006630d06f71e698
Gibran Iqbal <gibran.iqbal91@gmail.com>
2024-12-11 13:29:29 -0600

    refactor: removed qmgo dep in favor of mongo-driver official.

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.23.0
 require (
 	github.com/go-sql-driver/mysql v1.8.1
 	github.com/qiniu/qmgo v1.1.9
+	go.mongodb.org/mongo-driver v1.17.1
 	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
 )
 
@@ -21,7 +22,6 @@ require (
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
-	go.mongodb.org/mongo-driver v1.17.1 // indirect
 	golang.org/x/crypto v0.26.0 // indirect
 	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/sys v0.23.0 // indirect
--- mongo/main.go
+++ mongo/main.go
@@ -4,14 +4,15 @@ import (
 	"context"
 	"os"
 
-	"github.com/Jibbscript/dbminer-multi/dbminer"
-	"github.com/qiniu/qmgo"
-	"gopkg.in/mgo.v2/bson"
+	"github.com/jibbscript/dbminer-multi/dbminer"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type MongoMiner struct {
 	Host    string
-	session *qmgo.Client
+	session *mongo.Client
 }
 
 func New(host string) (*MongoMiner, error) {
@@ -24,27 +25,39 @@ func New(host string) (*MongoMiner, error) {
 }
 
 func (m *MongoMiner) connect() error {
-	s, err := qmgo.NewClient(context.Background(), &qmgo.Config{
-		Uri: m.Host,
-	})
+	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(m.Host))
 	if err != nil {
 		return err
 	}
-	m.session = s
+	m.session = client
 	return nil
+}
+
+func (m *MongoMiner) CollectionNames(dbName string) ([]string, error) {
+	db := m.session.Database(dbName)
+
+	filter := bson.D{}
+	collections, err := db.ListCollectionNames(context.Background(), filter)
+	if err != nil {
+		return nil, err
+	}
+
+	return collections, nil
 }
 
 func (m *MongoMiner) GetSchema() (*dbminer.Schema, error) {
 	var s = new(dbminer.Schema)
 
-	dbnames, err := m.session.DatabaseNames()
+	// Create a command to list databases
+	dbnames, err := m.session.ListDatabaseNames(context.Background(), bson.D{})
 	if err != nil {
 		return nil, err
 	}
 
 	for _, dbname := range dbnames {
 		db := dbminer.Database{Name: dbname, Tables: []dbminer.Table{}}
-		collections, err := m.session.DB(dbname).CollectionNames()
+
+		collections, err := m.CollectionNames(dbname)
 		if err != nil {
 			return nil, err
 		}
@@ -52,22 +65,18 @@ func (m *MongoMiner) GetSchema() (*dbminer.Schema, error) {
 		for _, collection := range collections {
 			table := dbminer.Table{Name: collection, Columns: []string{}}
 
-			var docRaw bson.Raw
-			err := m.session.DB(dbname).C(collection).Find(nil).One(&docRaw)
+			// Get a sample document to extract fields
+			var doc bson.M
+			err := m.session.Database(dbname).Collection(collection).FindOne(context.Background(), bson.M{}).Decode(&doc)
 			if err != nil {
-				return nil, err
+				continue // Skip this collection if we can't get a sample document
 			}
 
-			var doc bson.RawD
-			if err := docRaw.Unmarshal(&doc); err != nil {
-				if err != nil {
-					return nil, err
-				}
+			// Extract field names from the document
+			for fieldName := range doc {
+				table.Columns = append(table.Columns, fieldName)
 			}
 
-			for _, f := range doc {
-				table.Columns = append(table.Columns, f.Name)
-			}
 			db.Tables = append(db.Tables, table)
 		}
 		s.Databases = append(s.Databases, db)
@@ -76,7 +85,6 @@ func (m *MongoMiner) GetSchema() (*dbminer.Schema, error) {
 }
 
 func main() {
-
 	mm, err := New(os.Args[1])
 	if err != nil {
 		panic(err)
