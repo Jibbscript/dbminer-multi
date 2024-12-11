bc76c2bd18e2adcbdab60141dd3514b6907c1247
Gibran Iqbal <gibran.iqbal91@gmail.com>
2024-12-10 23:37:56 -0600

    feat: databaseminer interface stubbing

---
+++ dbminer/dbminer.go
@@ -0,0 +1,27 @@
+package dbminer
+
+type DatabaseMiner interface {
+	GetSchema() (*Schema, error)
+}
+
+type Schema struct {
+	Databases []Database
+}
+
+type Database struct {
+	Name   string
+	Tables []Table
+}
+
+type Table struct {
+	Name    string
+	Columns []string
+}
+
+func Search(m DatabaseMiner) error {
+	s, err := m.GetSchema()
+	if err != nil {
+		return err
+	}
+
+}
