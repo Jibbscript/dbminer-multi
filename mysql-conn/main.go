3a6f67ddcc5acd5e6c39d1ba1d9286f00ef80bdb
Gibran Iqbal <gibran.iqbal91@gmail.com>
2024-12-10 22:58:06 -0600

    feat: mysql connection driver test impl

---
+++ mysql-conn/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/store")
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer db.Close()
+
+	var (
+		ccnum, date, cvv, exp string
+		amount                float32
+	)
+
+	rows, err := db.Query("SELECT ccnum, date, amount, cvv, exp FROM transactions")
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		if err := rows.Scan(&ccnum, &date, &amount, &cvv, &exp); err != nil {
+			log.Panicln(err)
+		}
+		fmt.Println(ccnum, date, amount, cvv, exp)
+	}
+	if err := rows.Err(); err != nil {
+		log.Panicln(err)
+	}
+}
